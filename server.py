71d0bf2a9550575feeeea085ed6ecf65c6043c72
Imperson8 <56407616+Imperson8@users.noreply.github.com>
2022-04-08 09:25:37 +0300

    sfsd f
    ssadcv wf

--- server.py
+++ server.py
@@ -3,6 +3,20 @@ from flask import request
 from flask import render_template
 
 app = Flask(__name__)
+def loadAllPictures():
+  filepath = 'picDB.txt'
+  pictures=[]
+  with open(filepath) as fp:
+    line = fp.readline()
+    cnt = 1
+    while line:
+      if line!='':
+        fullPicInfo = line.strip().split(';')
+        pictures.append(fullPicInfo[1])
+      line = fp.readline()
+      cnt += 1
+  fp.close()
+  return pictures
 
 #Pirmā lapa, kas tiks ielādēta
 @app.route('/',methods = ['POST', 'GET'])
@@ -10,9 +24,11 @@ def root():
     return render_template("index.html")
     
 #Pārbaudes lapa, lai saprastu, ka kods vispār strādā
-@app.route('/health')
-def health():
-  return "OK"
+@app.route('/test',methods = ['POST', 'GET'])
+def test():
+    parametri = ["IQ","Augums","Māja"]
+    images = loadAllPictures()
+    return render_template("test.html",parametri=parametri,images=images)
 
 if __name__ == '__main__':
   app.run(debug="true")
